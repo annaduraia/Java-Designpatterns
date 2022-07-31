bf10ea66811723674cf3984165323d146ec8d2ed
annaduraia <annadurai.mca10@gmail.com>
2022-07-31 22:59:59 +0530

    Updated Notification services

--- Factory-method-design-pattern/src/NotificationService.java
+++ Factory-method-design-pattern/src/NotificationService.java
@@ -3,5 +3,12 @@ public class NotificationService {
 		NotificationFactory notificationFactory = new NotificationFactory();
 		Notification notification = (Notification) notificationFactory.createNotification("EMAIL");
 		notification.notifyUser();
+		
+		Notification notification1 = (Notification) notificationFactory.createNotification("SMS");
+		notification1.notifyUser();
+		
+		Notification notification2 = notificationFactory.createNotification("PUSH");
+		notification2.notifyUser();
+		
 	}
 }
