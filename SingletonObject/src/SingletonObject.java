92034a8f76b5d5dd265fedaf5c1b91f81e343c49
annaduraia <annadurai.mca10@gmail.com>
2022-07-31 10:08:32 +0530

    singleton design pattern

---
+++ SingletonObject/src/SingletonObject.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @author Annadurai A
+ *
+ */
+
+public class SingletonObject{
+
+	private static SingletonObject singletonObject;
+	
+	//1 private constructor
+	private SingletonObject()
+	{
+	}
+	
+	//2 getinstance method to restrict duplicate object
+	public  synchronized static SingletonObject getInstance()
+	{
+		if(null == singletonObject)
+			singletonObject = new SingletonObject();
+		return singletonObject;
+	}
+	//3 using clone method
+	public Object clone() throws CloneNotSupportedException
+	{
+		throw new CloneNotSupportedException();
+	}
+	
+}
---
+++ SingletonObject/src/mainSingletone.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Annadurai A
+ *
+ */
+
+import java.util.Calendar;
+
+public class mainSingletone {
+
+	public static void main(String[] args) {
+ 
+		//Singleton singleton = new Singleton();
+		
+		SingletonObject singleton = SingletonObject.getInstance();
+		
+		
+		Calendar calendar = Calendar.getInstance();
+		
+	}
+
+}
